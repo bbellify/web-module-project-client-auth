f481620dfd01ba3c01e7009e679cd57222baee91
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-11-09 13:23:06 -0800

    MVP met - need to style

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
 import Login from './components/Login'
 import FriendsList from './components/FriendsList';
 import NewFriendForm from './components/NewFriendForm'
+import Logout from './components/Logout'
 
 import PrivateRoute from './components/PrivateRoute';
 
@@ -12,10 +13,14 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <NavLink to='/logout'>Logout</NavLink>
+        </nav>
         <h2>Client Auth Project</h2>
         <Switch>
           <PrivateRoute exact path='/newfriend' component={NewFriendForm} />
           <PrivateRoute exact path='/friends' component={FriendsList} />
+          <PrivateRoute path='/logout' component={Logout} />
           <Route path='/' component={Login}/>
         </Switch>
       </div>
---
+++ friends/src/components/Logout.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const Logout = (props)=> {
+    const {push} = useHistory();
+
+    useEffect(()=> {
+
+        axiosWithAuth()
+            .post('/logout')
+            .then(resp => {
+                localStorage.removeItem('token');
+                push('/');
+            });
+    }, []);
+    
+    return(<div></div>);
+}
+
+export default Logout;
